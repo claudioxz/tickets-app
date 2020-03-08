d8f3ff89aee3843fa9a124d624518881a89743fa
Claudio <claudio.gomezquidel@gmail.com>
2020-03-08 02:28:57 -0300

    Migrations created

---
+++ database/migrations/2020_03_08_044847_ticket.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class Ticket extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('ticket', function (Blueprint $table) {
+            $table->increments('id');
+            $table->unsignedInteger('id_usuario');
+            $table->string('ticket_pedido');
+        });
+
+        Schema::table('ticket', function (Blueprint $table){
+            $table->foreign('id_usuario')->references('id')->on('usuario');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('ticket');
+    }
+}
